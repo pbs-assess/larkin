1906f88efb4fb81f91f0677635e93cf4e4d2be65
Luke A. Rogers <luke.allister.rogers@gmail.com>
2021-09-06 15:18:26 -0700

    updated documentation

--- R/data.R
+++ R/data.R
@@ -4,6 +4,7 @@
 #'
 #' @format A data frame with 1000 rows and 4 variables:
 #' \describe{
+#'   \item{Index}{Simulation time step}
 #'   \item{R_3}{abundance of age-3 recruitment}
 #'   \item{R_4}{abundance of age-4 recruitment}
 #'   \item{R_5}{abundance of age-5 recruitment}
