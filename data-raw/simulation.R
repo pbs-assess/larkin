2dafd49f64166f22c9830bb41e1b81b3f19eb9b1
Luke A. Rogers <luke.allister.rogers@gmail.com>
2021-09-06 15:18:40 -0700

    updated simulation data

--- data-raw/simulation.R
+++ data-raw/simulation.R
@@ -1,14 +1,14 @@
 # Simulate population dynamics from a Larkin model
 s1 <- sim(
   a = 7, # Per-capita population growth rate at low spawner abundance
-  b = rep(1, 4), # Density dependence parameters for spawners and lags
+  b = c(6, 2, 1, 1), # Density dependence parameters for spawners and lags
   init = rep(1, 8), # Initial spawner abundances
   h = 0.2, # Constant harvest rate
   p_bar = c(0.003, 0.917, 0.08), # Age at maturity mean proportions
-  omega = 0.6, # Age at maturity proportion deviation parameter
-  rho = 0.5, # Process error autocorrelation parameter
+  # omega = 0.6, # Age at maturity proportion deviation parameter
+  # rho = 0.5, # Process error autocorrelation parameter
   sigma = 0.8, # Process error standard deviation parameter
-  phi_1 = 0.1, # Initial process error deviation
+  # phi_1 = 0.1, # Initial process error deviation
   burn = 100, # Number of burn-in steps before the simulation
   span = 1000, # Number of steps in the simulation
   seed = 109 # Seed value for set.seed()
