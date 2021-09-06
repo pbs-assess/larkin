1690dc7abebf087270262365847c28c8614da1d9
Luke A. Rogers <luke.allister.rogers@gmail.com>
2021-09-06 15:19:29 -0700

    updated fit scripts

--- R/fit.R
+++ R/fit.R
@@ -5,8 +5,6 @@
 #' @param step_size [integer()] initial step size
 #' @param iter_warmup [integer()] number of warmup iterations
 #' @param iter_sampling [integer()] number of sampling iterations
-#' @param use_reduce_sum [logical()] use within chain parallel threading
-#' @param threads_per_chain [integer()] number of threads per chain
 #' @param ... additional arguments to pass to \code{$sample()} method
 #'
 #' @return [larkin()]
@@ -17,8 +15,6 @@ larkin <- function (data,
                     step_size = 0.1,
                     iter_warmup = 250,
                     iter_sampling = 750,
-                    use_reduce_sum = FALSE,
-                    threads_per_chain = 8,
                     ...) {
 
   # Check arguments ------------------------------------------------------------
@@ -29,8 +25,9 @@ larkin <- function (data,
   # Create model object
   mod <- cmdstanr::cmdstan_model(
     system.file("stan", "larkin.stan", package = "larkin"),
-    include_path = system.file("stan", package = "larkin"),
-    cpp_options = list(stan_threads = TRUE))
+    include_path = system.file("stan", package = "larkin")
+    # cpp_options = list(stan_threads = TRUE)
+  )
   # Fit the model
   cmdfit <- mod$sample(
     data = data,
@@ -38,17 +35,63 @@ larkin <- function (data,
     step_size = step_size,
     iter_warmup = iter_warmup,
     iter_sampling = iter_sampling,
-    threads_per_chain = threads_per_chain,
     ...
   )
-  # Convert to stanfit
-  samples <- rstan::read_stan_csv(cmdfit$output_files())
 
-  # Return mmmfit object -------------------------------------------------------
+  # Return larkin object -------------------------------------------------------
 
   structure(list(
     data = data,
-    samples = samples,
-    cmdfit = cmdfit),
+    output = cmdfit$output_files(),
+    summary = cmdfit$summary()),
     class = "larkin")
+}
+
+#' Fit A Ricker Model Via CmdStanR
+#'
+#' @param data [list()]
+#' @param chains [integer()] number of chains
+#' @param step_size [integer()] initial step size
+#' @param iter_warmup [integer()] number of warmup iterations
+#' @param iter_sampling [integer()] number of sampling iterations
+#' @param ... additional arguments to pass to \code{$sample()} method
+#'
+#' @return [ricker()]
+#' @export
+#'
+ricker <- function (data,
+                    chains = 1,
+                    step_size = 0.1,
+                    iter_warmup = 250,
+                    iter_sampling = 750,
+                    ...) {
+
+  # Check arguments ------------------------------------------------------------
+
+
+  # Fit the model via cmdstanr -------------------------------------------------
+
+  # Create model object
+  mod <- cmdstanr::cmdstan_model(
+    system.file("stan", "ricker.stan", package = "larkin"),
+    include_path = system.file("stan", package = "larkin")
+    # cpp_options = list(stan_threads = TRUE)
+  )
+  # Fit the model
+  cmdfit <- mod$sample(
+    data = data,
+    chains = chains,
+    step_size = step_size,
+    iter_warmup = iter_warmup,
+    iter_sampling = iter_sampling,
+    ...
+  )
+
+  # Return ricker object -------------------------------------------------------
+
+  structure(list(
+    data = data,
+    output = cmdfit$output_files(),
+    summary = cmdfit$summary()),
+    class = "ricker")
 }
