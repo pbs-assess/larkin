0d3004848e97288b12b2b62680f135570b86958a
carrieholt <Carrie.Holt@dfo-mpo.gc.ca>
2023-07-28 09:17:20 -0700

    revised optimize so forecast gives MLE estimate

--- R/functions.R
+++ R/functions.R
@@ -493,7 +493,7 @@ forecast <- function (data,
         dplyr::bind_cols(id_columns) %>%
         dplyr::relocate(colnames(id_columns), .before = 1) %>%
         dplyr::mutate(method = "model") %>%
-        dplyr::mutate(mean = NA) %>%
+        dplyr::mutate(mean = fo) %>%
         dplyr::mutate(median = NA) %>%
         dplyr::mutate(sd = NA) %>%
         dplyr::mutate(mad = NA) %>%
